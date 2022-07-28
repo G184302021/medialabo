d94cf003a0e88d19957fc14321f81d8a773a4525
G184302021 <218430@st.takushoku-u.ac.jp>
2022-07-29 00:21:01 +0900

    saishukai

--- gourmet.js
+++ gourmet.js
@@ -1,17 +1,30 @@
 
-let b = document.querySelector('#print');
-b.addEventListener('click', gourmet);
-let li = document.querySelector('span#access');
-let li2 = document.querySelector('span#address');
-// let li3 = document.querySelector('span#budget');
-// let li4 = document.querySelector('span#catch');
-// let li5 = document.querySelector('span#genre');
-// let li6 = document.querySelector('span#neme');
-// let li7 = document.querySelector('span#open');
-// let li8 = document.querySelector('span#station');
-// let li9 = document.querySelector('span#sub');
+let x = document.querySelector('#print');
+x.addEventListener('click', gourmet);
+let m=0;
+
+let ul = document.querySelector('div#shop');
+    let li;
+    let a;
+
+    let u = document.createElement('div');
+   u.classList.add('kekka');
+
+
+   
+
+      let v =document.querySelector('span#nai');
+      // let r = document.createElement('div');
+      // r.classList.add('r');
+
+
 
 function gourmet(){
+
+  u.remove();
+  
+  
+
   let i = document.querySelector('input[name="chose"]');
   genre = i.value;
   // genre = document.querySelector('#print');
@@ -25,6 +38,8 @@ function gourmet(){
 
 }
 function showResult(resp){
+  
+  
    // サーバから送られてきたデータを出力
    let data = resp.data;
 
@@ -39,13 +54,74 @@ function showResult(resp){
    // data.x を出力
    console.log(data.x);
 
+   
+    
+    let z = document.querySelector('span#genre');
+    
 
-  for (let n of data.results.shop){
-    // li.textContent=n.access;
-    // li2.textContent=n.address;
-    console.log(n.access);
+    for (let n of data.results.shop){
+      m=m+1;
+      
 
-  }
+      
+    
+      z.textContent = n.genre.name;
+
+      a = document.createElement('a');
+      a.textContent=n.name;;
+      u.insertAdjacentElement('beforeend', a);
+      
+
+      li = document.createElement('li');
+      li.textContent = "キャッチ："+n.catch;
+      u.insertAdjacentElement('beforeend', li);
+
+      li = document.createElement('li');
+      li.textContent = '営業時間：'+n.open;
+      u.insertAdjacentElement('beforeend', li);
+
+      li = document.createElement('li');
+      li.textContent = 'ジャンル：'+n.genre.name;
+      u.insertAdjacentElement('beforeend', li);
+
+      li = document.createElement('li');
+      li.textContent = '予算：'+n.budget.name;
+      u.insertAdjacentElement('beforeend', li);
+
+      li = document.createElement('li');
+      li.textContent = '住所：'+n.address;
+      u.insertAdjacentElement('beforeend', li);
+
+      li = document.createElement('li');
+      li.textContent = 'アクセス：'+n.access;
+      u.insertAdjacentElement('beforeend', li);
+
+      li = document.createElement('li');
+      li.textContent = '最寄り駅：'+n.station_name;
+      u.insertAdjacentElement('beforeend', li);
+
+      li = document.createElement('li');
+      li.textContent = 'サブジャンル：'+n.sub_genre.name;
+      u.insertAdjacentElement('beforeend', li);
+
+      ul.insertAdjacentElement('beforeend', u);
+
+      if (m>1){
+        v.textContent =' ';
+      }
+    }
+    if (m===0){
+      
+      // t = document.createElement('t');
+      // t.textContent = 'ありません。';
+      // r.insertAdjacentElement('beforeend', t);
+      // v.insertAdjacentElement('beforeend',r);
+      v.textContent = 'ありません。';
+      
+
+    }
+
+    
 
 
 
@@ -61,8 +137,23 @@ function showError(err) {
 // 通信の最後にいつも実行する処理
 function finish() {
 	console.log('Ajax 通信が終わりました');
+
+
 }
 
+
+
+
+// li = document.createElement('li2');
+//     li.textContent = n.access;
+//     ul.insertAdjacentElement('beforeend',li);
+//     // li.textContent=n.name;
+//     // li1.textContent=n.access;
+//     // li2.textContent=n.address;
+//     // li3.textContent=n.budget.name;
+//     // li4.textContent=n.name;
+
+//     console.log(n.access);
 
 // let i = document.querySelector('input[name="chose"]');
 //   let li = document.querySelector('span#shop');
